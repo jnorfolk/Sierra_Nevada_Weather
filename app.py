a0042a5decf99759549e7e11317a1df7338b1b86
Josh <joshua.quinn.norfolk@gmail.com>
2023-03-16 10:17:31 -0400

    Complete data preprocessing

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly
+import matplotlib.pyplot as plt
+
+st.header("Weather in the Sierra Nevada")
+
+st.write("It is not a functional app yet. Under construction.")
